be6475a0db4e2d0d1b8437c50143f02198494e29
nspringwater <nss2141@columbia.edu>
2015-09-23 11:50:03 -0400

    made playToFive function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,20 +25,18 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    var getPlayerMove = move || getInput();
-    return getPlayerMove;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    var getComputerMove = move || randomPlay();
-    return getComputerMove;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
+    var winner = null;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
@@ -68,6 +66,20 @@ function playToFive() {
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+
+    while (playerWins < 5 && computerWins < 5) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner();
+        if (winner === 'player') {
+            playerWins++;
+        }
+        else {
+            computerWins++;
+        }
+        console.log('Player chose ' + playerMove + ' and Computer chose' + computerMove);
+        console.log('The score is currently - Player: ' + playerWins ', Computer: ' + computerWins);
+    }
     return [playerWins, computerWins];
 }
 
